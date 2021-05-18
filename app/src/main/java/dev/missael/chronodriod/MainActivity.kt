beb009d1140abe7fc91a0eca8714670898d7062b
mhernandez <mhernandez@idisc.com>
2021-05-18 18:08:41 -0500

    initial commit

---
+++ app/src/main/java/dev/missael/chronodriod/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.missael.chronodriod
+
+import android.app.Activity
+import android.os.Bundle
+import dev.missael.chronodriod.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
